141831eb8ca1288c9dc1b4d568c38dbc67975620
Ahad Hossain <hossain1814@gmail.com>
2020-08-13 17:19:04 +0600

    Promisify writeFile function

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const readFilePro = file => {
     });
 }
 
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(file, data, err => {
+            if(err) reject('Could not write the file');
+            resolve('success');
+        });
+    });
+}
+
 readFilePro(`${__dirname}/dog.txt`).then(data => {
     console.log(`Breed:${data}`);
     
