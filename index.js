d62f33993c401f626bb58eac3e9f2b24f72882fb
Ahad Hossain <hossain1814@gmail.com>
2020-08-13 18:41:14 +0600

    Use async/await fomula for handling promises. js ES8 syntax

--- index.js
+++ index.js
@@ -19,15 +19,15 @@ const writeFilePro = (file, data) => {
     });
 }
 
-readFilePro(`${__dirname}/dog.txt`)
- .then(data => {
-     console.log(`Breed:${data}`);
-     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
- }).then(res=> {
-     console.log(res.body.message);
-     return writeFilePro('dog-text.txt', res.body.message)
- }).then(()=> {
-     console.log('Random dog image saved');
- }).catch(err => {
-     console.log(err.message)
-});
+const getDogPic = async() => {
+    const data = await readFilePro(`${__dirname}/dog.txt`)
+    console.log(`Breed:${data}`);
+
+    const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    console.log(res.body.message);
+
+    await writeFilePro('dog-text.txt', res.body.message);
+    console.log('Random dog image saved');
+}
+
+getDogPic();
