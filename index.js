946e7cf41930c8fb324e2f967fdbfa29483e6e8d
Ahad Hossain <hossain1814@gmail.com>
2020-08-13 13:47:41 +0600

    callback inside call back inside call back

--- index.js
+++ index.js
@@ -7,5 +7,9 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
         console.log(res.body.message);
+        
+        fs.writeFile('dog-text.txt', res.body.message, err => {
+            console.log('Random dog image saved');
+        })
     })
 });
